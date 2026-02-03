4e62cca55ce502fe48bc4388abafff975df324b6
Riteshm-07 <147985146+Riteshm-07@users.noreply.github.com>
2026-02-04 01:15:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// Text Animation for Role (Web Developer)
+const roles = [
+  "Web Developer",
+  "UI/UX Designer",
+  "Front-End Developer",
+  "Back-End Developer"
+];
+
+const h1 = document.querySelector(".content h1");
+const originalHTML = h1.innerHTML;
+
+// Extract the role text node
+let roleSpan = document.createElement("span");
+roleSpan.style.color = "#ffffff";
+roleSpan.style.borderRight = "2px solid #00e5ff";
+roleSpan.style.paddingRight = "5px";
+
+h1.innerHTML = originalHTML.replace("Web Developer", "");
+h1.appendChild(roleSpan);
+
+let roleIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+  const currentRole = roles[roleIndex];
+
+  if (!isDeleting) {
+    roleSpan.textContent = currentRole.substring(0, charIndex + 1);
+    charIndex++;
+    if (charIndex === currentRole.length) {
+      setTimeout(() => (isDeleting = true), 1500);
+    }
+  } else {
+    roleSpan.textContent = currentRole.substring(0, charIndex - 1);
+    charIndex--;
+    if (charIndex === 0) {
+      isDeleting = false;
+      roleIndex = (roleIndex + 1) % roles.length;
+    }
+  }
+
+  setTimeout(typeEffect, isDeleting ? 80 : 120);
+}
+
+typeEffect();
